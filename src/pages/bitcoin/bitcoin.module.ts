e6711573bef4bbcc02760c6a17c148315f99bc6f
“Sam <sammans@Sams-MBP.fios-router.home>
2018-11-15 07:51:03 -0500

    Fixing small error

--- src/pages/bitcoin/bitcoin.module.ts
+++ src/pages/bitcoin/bitcoin.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { BitcoinPage } from './bitcoin';
 import { BitcoinProvider } from '../../providers/bitcoin/bitcoin';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
 /*Had to import this because HttpClient was changed https://stackoverflow.com/questions/46000748/error-no-provider-for-httphandler-in-angular2*/
 
 
